d3a43d826fe46fbbf71055f9b9ec5bb48101b48b
SimbaScorpio <425830509@qq.com>
2021-02-20 02:07:03 +0800

    Getting started/Camera

--- src/shader.h
+++ src/shader.h
@@ -2,7 +2,10 @@
 #define SHADER_H
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
-  
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <string>
 #include <fstream>
 #include <sstream>
@@ -20,9 +23,10 @@ public:
     // use/activate the shader
     void use();
     // utility uniform functions
-    void setBool(const std::string &name, bool value) const;  
-    void setInt(const std::string &name, int value) const;   
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
     void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, glm::mat4 value) const;
 private:
     // utility function for checking shader compilation/linking errors.
     void checkCompileErrors(unsigned int shader, std::string type);
